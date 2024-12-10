99894ea4e2aa457c3fb8410e0851e97f785d41e0
Gabriel Jaya <gabriel.jaya@epn.edu.ec>
2024-12-09 19:06:30 -0500

    Update Pantalla Meta

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,15 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     showPage(1); // Mostrar la primera página al cargar la página
+    setupTabs(); // Configurar eventos para las pestañas
 });
 
+function setupTabs() {
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function() {
+            const pageNumber = parseInt(tab.dataset.page); // Obtiene el número de la pestaña
+            showPage(pageNumber);
+        });
+    });
+}
+
 function showPage(pageNumber) {
-    // Ocultar todas las páginas
-    var pages = document.querySelectorAll('.page');
+    // Ocultar todas las secciones
+    const pages = document.querySelectorAll('.page');
     pages.forEach(function(page) {
         page.style.display = 'none';
     });
 
-    // Mostrar la página seleccionada
-    var pageToShow = document.getElementById('page' + pageNumber);
-    pageToShow.style.display = 'flex';
+    // Mostrar la sección correspondiente
+    const pageToShow = document.getElementById('page' + pageNumber);
+    if (pageToShow) {
+        pageToShow.style.display = 'flex';
+    }
 }
+
+
+
+
