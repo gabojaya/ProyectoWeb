1082998a1a0c3f461fd2e8e448e89e90f580d1f2
Gabriel Jaya <gabriel.jaya@epn.edu.ec>
2024-12-09 20:19:57 -0500

    Update Pantalla RegistrarMeta

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     showPage(1); // Mostrar la primera página al cargar la página
     setupTabs(); // Configurar eventos para las pestañas
+    setupMetaScreen();
 });
 
 function setupTabs() {
@@ -27,6 +28,27 @@ function showPage(pageNumber) {
     }
 }
 
+function setupMetaScreen() {
+    const addMetaBtn = document.getElementById('add-meta-btn');
+    const screenOverlay = document.getElementById('screenOverlay');
+    const cerrarBtn = document.getElementById('cerrar-btn');
 
+    // Funcionalidad para mostrar la pantalla modal al hacer clic en "Agregar Meta"
+    addMetaBtn.addEventListener('click', function() {
+        screenOverlay.style.display = 'flex'; // Mostrar la pantalla modal
+    });
 
+    // Funcionalidad para ocultar la pantalla modal al hacer clic en "Cancelar"
+    cerrarBtn.addEventListener('click', function() {
+        screenOverlay.style.display = 'none'; // Ocultar la pantalla modal
+    });
 
+    // Funcionalidad para manejar los botones de "Editar Meta"
+    const editarMetaBtns = document.querySelectorAll('.editar-meta');
+    editarMetaBtns.forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            screenOverlay.style.display = 'flex'; // Mostrar la pantalla modal
+            // Opcional: Aquí puedes agregar lógica para cargar datos dinámicos
+        });
+    });
+}
